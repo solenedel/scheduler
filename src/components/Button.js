45b702c857add8c3fc4565321632db7c8b81a64a
Solene Delumeau <solene@Solenes-MacBook-Pro.local>
2021-08-31 13:22:04 -0700

    add DayListItem component

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,29 @@ import React from "react";
 
 import "components/Button.scss";
 
+import classNames from "classnames";
+import { removePropertiesDeep } from "@babel/types";
+
+
+
 export default function Button(props) {
-   return <></>;
+
+   const classnames = require("classnames");
+
+   const buttonClass = classnames("button", {
+      "button--confirm": props.confirm, 
+      "button--danger": props.danger
+   });
+
+
+   return (
+         <button 
+         className={buttonClass}
+         onClick={props.onClick}
+         disabled={props.disabled}
+         >{props.children}
+         </button>
+   );
+  
+
 }
