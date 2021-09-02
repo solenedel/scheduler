7996298beef4c2ec3be365e1d5c404ed08353b2c
Solene Delumeau <solene@Solenes-MacBook-Pro.local>
2021-09-01 21:30:11 -0700

    added state to the form, fixed bug to select interviewer

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -7,15 +7,33 @@ export default function Form(props) {
   // set default states for interviewer and name
 
   // interviewer state
-  const [interviewer, setInterviewer] = useState(null);
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
   // name state
-  const [name, setName] = useState("");
+  
+  const [name, setName] = useState(props.name || "");
+
+  // the props.name is any hardcoded value we have in the test code
+
+  // HELPER FUNCTIONS
+  // reset: clears the student name entered in the form and 
+  // deselects the currently selected interviewer
+  const reset = () => {
+    setName("");
+    setInterviewer(null);
+  };
+
+  // cancel will call reset and cancel
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
 
 
   return (
     <main className="appointment__card appointment__card--create">
-      <section className="appointment__card-left">
+      <section className="appointment__card-left"
+               onSubmit={event => event.preventDefault()}>
         <form autoComplete="off">
           <input
             className="appointment__create-input text--semi-bold"
@@ -23,12 +41,12 @@ export default function Form(props) {
             type="text"
             // the name below references the first element of the array on line 10
             // set the input value attribute to the current name state
-            // value={name} 
             value={name}
             placeholder="Enter Student Name"
-            // every time there is a new change in the input, the component will re-render
-            // for every change in the name value, and set the value to the new {name}
+            // e.target.value corresponds to the current input value
             onChange={(e) => setName(e.target.value)}
+          // value={name} and onChange={(e) => setName(e.target.value)} turn the Form 
+          // component into a controlled component
           />
         </form>
         <InterviewerList 
@@ -38,7 +56,7 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
           <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
