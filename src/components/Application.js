c5ce28c48cb47a85d2b6985b228cf549fad4e857
Solene Delumeau <solene@Solenes-MacBook-Pro.local>
2021-09-03 12:42:56 -0700

    add request to api/appointments and rmake changes to Application component function

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,90 +4,48 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
+import { getAppointmentsForDay } from "helpers/selectors";
 import "./Application.scss";
 
 // ------------------------- Test Data ------------------------- //
-
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-    interview: {
-      student: "Bob Thomas",
-      interviewer: {
-        id: 2,
-        name: "Tori Malcolm",
-        avatar: "https://i.imgur.com/Nmx0Qxo.png" 
-      }
-    } 
-  },
-
-  {
-    id: 4,
-    time: "4pm",
-  },
-    
-  {
-    id: 5,
-    time: "4:30pm",
-    interview: {
-      student: "Gary Jipp",
-      interviewer: {
-        id: 5,
-        name: "Sven Jones",
-        avatar: "https://i.imgur.com/twYrpay.jpg" 
-      }
-    } 
-  }
-  
-];
 
 
 // -------------------------- FUNCTION: Application component --------------------------- //
 
 export default function Application(props) {
 
+  // object to store states
   const [state, setState] = useState({
     day: "Monday", 
     days: [], 
-     // you may put the line below, but will have to remove/comment hardcoded appointments variable
     appointments: {}
   });
 
-  const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({ ...prev, days }));
+  console.log('state: ', state);
 
-
-  // below is not needed because of the above state object
-  // const [day, setDay] = useState(["Monday"]);
-  // const [days, setDays] = useState([]);
-  // const [appointments, setAppointments] = useState({});
-
-  // request days data from /api/days
+  // request days and appointments data. The promise resolves when both
+  // get requests are complete.
   useEffect(() => {
-    axios.get('/api/days').then(response => {
-      // console.log('response.data:', response.data);
-      setDays([...response.data]);
-    })
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments')
+    ]).then(response => {
+        setState(prev => {
+          return {
+            ...prev,
+            days: response[0].data,
+            appointments: response[1].data
+          };
+        });
+    }).catch(response => console.log('Error: ', response.message));
   }, []);
 
+  // call function to get the appointments for a certain day depending on the
+  // state of the selected day
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   // JSX to be returned by the component function
   return (
@@ -115,7 +73,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {appointments.map(appointment => {
+        {dailyAppointments.map(appointment => {
           return (
             <Appointment
               key={appointment.id}
@@ -130,4 +88,5 @@ export default function Application(props) {
     </main>
   );
 
+  // end of Application component function 
 }
