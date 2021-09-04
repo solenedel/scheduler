c3da83ad82a86307a40c38cc70f093fc925b6090
Solene Delumeau <solene@Solenes-MacBook-Pro.local>
2021-09-03 17:39:24 -0700

    add transition and back function to useVisualMode, add tests

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,9 +1,30 @@
 function useVisualMode(initial) {
 
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  return { mode };
+  // transition to the next mode
+  const transition = (mode, replace = false) => {
+    mode = setMode(mode);
+  }
+
+  // go back to the previous mode
+  const back = history => {
+
+    if (history.length >= 1) {
+      // remove last item from history array, without mutating original history array
+      const lastItem = history.slice(0, -1);
+      console.log('lastItem: ', lastItem);
+
+      history = setHistory(lastItem);
+    }
+    
+  }
+
+  return { mode, transition, back };
+
 
 }
 
+// module.exports = {useVisualMode};
 export {useVisualMode};
