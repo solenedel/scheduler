b053834a7e4d8af49337d9aa3adad570ba7a0215
Solene Delumeau <solene@Solenes-MacBook-Pro.local>
2021-09-04 10:44:47 -0700

    add conditional rendering of Form to Application component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,6 +3,7 @@ import "./styles.scss";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import Form from "components/Appointment/Form";
 import { useVisualMode } from "hooks/useVisualMode";
 
 
@@ -10,10 +11,13 @@ export default function Appointment(props) {
 
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
 
   // store a snapshot of the initial state on first page load
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
+
   );
   
 
@@ -23,10 +27,11 @@ export default function Appointment(props) {
       <Header time={props.time} />
       {/* if mode is EMPTY, render EMPTY component. 
       if mode is SHOW, render SHOW component. */}
-      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && ( 
        <Show student={props.interview.student} 
-             interviewer={props.interview.interviewer} /> )}                           
+             interviewer={props.interview.interviewer} /> )} 
+      {mode === CREATE && <Form interviewers={props.interviewers} onCancel={() => back()} />}                          
     </article>
 
   );
