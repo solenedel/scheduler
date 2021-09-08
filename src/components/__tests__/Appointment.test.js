6e58699c46f8dc7351c3ae2a0ca8d8ac98e70a29
solenedel <solene98@student.ubc.ca>
2021-09-08 11:00:45 -0700

    fix import in Appointment.test.js, renders without crashing

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -1,22 +1,19 @@
-/*
-  We are rendering `<Application />` down below, so we need React.createElement
-*/
+
+// We are rendering `<Application />` down below, so we need React.createElement
 import React from "react";
 
-/*
-  We import our helper functions from the react-testing-library
-  The render function allows us to render Components
-*/
+// We import our helper functions from the react-testing-library
+// The render function allows us to render Components
 import { render } from "@testing-library/react";
 
-/*
-  We import the component that we are testing
-*/
-import Application from "components/Application";
+// We import the component that we are testing
+import Appointment from "components/Application";
 
-/*
-  A test that renders a React Component
-*/
-it("renders without crashing", () => {
-  render(<Application />);
+
+// A test that renders a React Component
+
+describe("Appointment", () => {
+  it("renders without crashing", () => {
+    render(<Appointment />);
+  });
 });
