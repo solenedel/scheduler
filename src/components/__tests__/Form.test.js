5bf5a77b63457476e414feab68557a7d202e2ace
solenedel <solene98@student.ubc.ca>
2021-09-08 12:56:37 -0700

    add to Form.test.js, tests passed

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { render, cleanup } from "@testing-library/react";
+import { getByPlaceholderText, getByTestId } from "@testing-library/dom";
 import Form from "components/Appointment/Form";
 
 afterEach(cleanup);
 
 describe("Form", () => {
+
   const interviewers = [
     {
       id: 1,
@@ -13,11 +15,14 @@ describe("Form", () => {
     }
   ];
 
+
   it("renders without student name if not provided", () => {
+    const { getByPlaceholderText } = render(<Form interviewers={interviewers} />);
     expect(getByPlaceholderText("Enter Student Name")).toHaveValue("");
   });
 
   it("renders with initial student name", () => {
+    const { getByTestId } = render(<Form interviewers={interviewers} name="Lydia Miller-Jones" />);
     expect(getByTestId("student-name-input")).toHaveValue("Lydia Miller-Jones");
   });
 });
