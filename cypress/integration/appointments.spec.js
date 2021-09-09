e0321fed3e6d3199d326fd0e52e187d8c24595d2
solenedel <solene98@student.ubc.ca>
2021-09-08 18:44:41 -0700

    add test: should edit an interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -8,13 +8,34 @@ describe("appointments", () => {
    });
  
 
+   // test steps
   it('should book an interview', () => {
-    cy.get('[alt=Add]').first().click();
+    cy.get('[alt=Add]')
+    .first()
+    .click();
+
     cy.get('[data-testid=student-name-input]').type('Lydia Miller-Jones')
     cy.get('[alt="Sylvia Palmer"]').click();
+
     cy.contains('Save').click();
+
     cy.contains('.appointment__card--show', 'Lydia Miller-Jones')
     .contains('.appointment__card--show', 'Sylvia Palmer');
   });
+
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]")
+      .first()
+      .click({ force: true });
+  
+    cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");
+    cy.get("[alt='Tori Malcolm']").click();
+  
+    cy.contains("Save").click();
+  
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
+
 
 });
