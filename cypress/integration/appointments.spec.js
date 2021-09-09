4a5f8dfbdb3b28937f3f3d1abdf868ee139ddcd4
solenedel <solene98@student.ubc.ca>
2021-09-08 18:41:37 -0700

    add test to appointments.spec.js: should book an interview

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,20 @@
+describe("appointments", () => {
+  
+  // reset database, visit root 
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
+    cy.visit("/");
+    cy.contains("Monday");
+   });
+ 
+
+  it('should book an interview', () => {
+    cy.get('[alt=Add]').first().click();
+    cy.get('[data-testid=student-name-input]').type('Lydia Miller-Jones')
+    cy.get('[alt="Sylvia Palmer"]').click();
+    cy.contains('Save').click();
+    cy.contains('.appointment__card--show', 'Lydia Miller-Jones')
+    .contains('.appointment__card--show', 'Sylvia Palmer');
+  });
+
+});
