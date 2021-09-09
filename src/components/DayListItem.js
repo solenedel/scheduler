b2077ccee0e77ba9f51767222b767e2994b1fdc6
solenedel <solene98@student.ubc.ca>
2021-09-08 18:28:31 -0700

    pass cypress tests in navigation.spec.js

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -36,8 +36,9 @@ export default function DayListItem(props) {
       className={dayClass}
       // run setDay with the HTML name (day of the week) of the component we clicked on
       // setDay is defined in Application.js as a prop 
-      onClick={() => props.setDay(props.name)}>
-
+      onClick={() => props.setDay(props.name)}
+      data-testid="day">
+      
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots()}</h3>
     </li>
